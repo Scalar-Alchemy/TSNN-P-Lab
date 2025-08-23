1f4f254a635a231203fcd0f89a5ca3dfbc12057e
Scalar-Alchemy <scalar.alch@gmail.com>
2025-08-23 13:01:35 +1200

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-
-```bash
-#!/bin/bash
-echo "Setting up TSNN-P environment..."
-pip install --no-cache-dir -r requirements.txt
-python -c "import cupy; print('CuPy version:', cupy.__version__)"
-echo "Setup complete!"
-```
